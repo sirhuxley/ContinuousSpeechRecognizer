0d676c1054abb7c7c388895e60990d3658665283
sirhuxley <d.huiskens@gmx.net>
2015-03-24 14:46:56 +0100

    free speech

--- src/android/ContinuousSpeechRecognizer.java
+++ src/android/ContinuousSpeechRecognizer.java
@@ -106,8 +106,8 @@ public class ContinuousSpeechRecognizer extends CordovaPlugin {
         }
 
         intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-        //intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
-		intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,RecognizerIntent.LANGUAGE_MODEL_WEB_SEARCH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+		//intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,RecognizerIntent.LANGUAGE_MODEL_WEB_SEARCH);
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, language);
         intent.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE,"voice.recognition.test");
 		maxMatches = 1;
